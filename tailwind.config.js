5b865981b636e3f26458bfc592b9b52aca384b01
Darragh ORiordan <darragh.oriordan@gmail.com>
2022-09-03 21:49:40 +1000

    tailwind working again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ['./app/**/*.{js,jsx,ts,tsx}', './index.html'],
+    content: ['./src/app/**/*.{js,jsx,ts,tsx}', './src/public/index.html'],
   },
   // darkMode: false, // or 'media' or 'class'
   theme: {
