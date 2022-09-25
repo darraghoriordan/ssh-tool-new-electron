e2d11ec1ccc64789e99f4859f43ee827e096dcd7
Darragh O'Riordan <darragh.oriordan@gmail.com>
2022-09-25 23:47:22 +1000

    updated some windows requirements

--- src/electron/appSettings/models/DefaultSettingsWindows.ts
+++ src/electron/appSettings/models/DefaultSettingsWindows.ts
@@ -6,8 +6,8 @@ export class DefaultSettingsWindows extends ApplicationSettings {
   constructor() {
     super()
     this.platformMatcher = 'win32'
-    this.projectsPath = '~/projects'
-    this.sshCertPath = '~/.ssh'
+    this.projectsPath = 'C:\\projects'
+    this.sshCertPath = path.join(app.getPath('home'), '.ssh')
     this.sshConfigFilePath = path.join(app.getPath('home'), '.ssh', 'config')
     this.globalGitConfigFile = path.join(app.getPath('home'), '.gitconfig')
   }
