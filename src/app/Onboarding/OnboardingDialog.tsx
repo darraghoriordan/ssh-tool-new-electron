64e069cb5fdaab885933d2920810b58be063744b
Darragh ORiordan <darragh.oriordan@gmail.com>
2022-10-01 01:01:04 +1000

    feat: added semantic releases

--- src/app/Onboarding/OnboardingDialog.tsx
+++ src/app/Onboarding/OnboardingDialog.tsx
@@ -1,28 +1,21 @@
 import { Dialog, Transition } from '@headlessui/react'
 import { CommandLineIcon } from '@heroicons/react/24/outline'
 import { Fragment } from 'react'
-import { useNavigate } from 'react-router'
-import { useSetFirstUsageDate } from '../AppSettings/ReactQueryWrappers'
 
 export class OnboardingDialogProps {
-  open!: boolean
+  closed!: boolean
+  setClosed!: (closed: boolean) => void
 }
-export default function OnboardingDialog() {
-  const navi = useNavigate()
-  const setFirstUsage = useSetFirstUsageDate()
-  const close = () => {
-    setFirstUsage
-      .mutateAsync()
-      .catch
-      // ignoring this for now, the user will just get the popup next time if something is wrong here
-      ()
-      .finally(() => {
-        navi('/settings')
-      })
-  }
+export default function OnboardingDialog(props: OnboardingDialogProps) {
   return (
-    <Transition.Root show={true} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={() => {}}>
+    <Transition.Root show={!props.closed} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => {
+          props.setClosed(true)
+        }}
+      >
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -48,9 +41,9 @@ export default function OnboardingDialog() {
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                 <div>
-                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-pink-300">
                     <CommandLineIcon
-                      className="h-6 w-6 text-pink-400"
+                      className="h-6 w-6 text-blue-500"
                       aria-hidden="true"
                     />
                   </div>
@@ -77,7 +70,9 @@ export default function OnboardingDialog() {
                   <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
-                    onClick={() => close()}
+                    onClick={() => {
+                      props.setClosed(true)
+                    }}
                   >
                     Go to the settings page now
                   </button>
--- src/app/Onboarding/OnboardingScreen.tsx
+++ src/app/Onboarding/OnboardingScreen.tsx
@@ -1,13 +1,49 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSetFirstUsageDate } from '../AppSettings/ReactQueryWrappers'
 import PageHeader from '../components/PageHeader'
 import OnboardingDialog from './OnboardingDialog'
 
 export function OnboardingScreen() {
+  const navigation = useNavigate()
+  const setFirstUsage = useSetFirstUsageDate()
+  const [closed, setClosed] = useState(false)
+
+  useEffect(() => {
+    if (
+      closed &&
+      !setFirstUsage.isLoading &&
+      setFirstUsage.failureCount <= 0 &&
+      !setFirstUsage.isSuccess &&
+      !setFirstUsage.isError
+    ) {
+      setFirstUsage.mutateAsync(undefined, {
+        onSuccess: () => {
+          console.log('navigation in on success')
+          navigation('/settings')
+        },
+      })
+      console.log('running in useEffect')
+      return () => {
+        console.log('running in useEffect return')
+        navigation('/settings')
+      }
+    }
+    return
+  }, [
+    closed,
+    setFirstUsage.isLoading,
+    setFirstUsage.failureCount,
+    setFirstUsage.isSuccess,
+    setFirstUsage.isError,
+    navigation,
+  ])
+
   return (
     <div className="max-w-10xl mx-auto">
       <PageHeader pageTitle={''}></PageHeader>
-      <OnboardingDialog />
+      <OnboardingDialog closed={closed} setClosed={setClosed} />
     </div>
   )
 }
