65c4f6d43d59f83a79e573ed629291f936df3563
Darragh O'Riordan <darragh.oriordan@gmail.com>
2022-10-09 18:25:28 +1100

    use digiocean storage

--- src/electron/licencing/buildSettings.ts
+++ src/electron/licencing/buildSettings.ts
@@ -1,4 +1,4 @@
 // The contents of this file are overwritten by the release process.
 // *********************************************************************
-export const buildDate = '2022-10-09T14:18:20+11:00'
+export const buildDate = '2022-10-09T15:28:29+11:00'
 export const useStoreLicence = false
