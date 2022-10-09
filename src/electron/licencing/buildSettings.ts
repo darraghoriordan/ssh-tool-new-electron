d4fe22e49a9ac4b97ab3f689721c27f4566f0031
Darragh ORiordan <darragh.oriordan@gmail.com>
2022-10-09 22:18:22 +1100

    fix: the mac build script

--- src/electron/licencing/buildSettings.ts
+++ src/electron/licencing/buildSettings.ts
@@ -1,4 +1,4 @@
 // The contents of this file are overwritten by the release process.
 // *********************************************************************
-export const buildDate = '2022-10-09T15:28:29+11:00'
+export const buildDate = '2022-10-09T11:15:47Z'
 export const useStoreLicence = false
