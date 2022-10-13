d4e8c8d3cffaf523374dd515afef6b576c4c385b
Darragh ORiordan <darragh.oriordan@gmail.com>
2022-10-13 23:33:07 +1100

    fix: add mac output back to digital ocean

--- src/electron/licencing/buildSettings.ts
+++ src/electron/licencing/buildSettings.ts
@@ -1,4 +1,4 @@
 // The contents of this file are overwritten by the release process.
 // *********************************************************************
-export const buildDate = '2022-10-13T11:12:35Z'
-export const useStoreLicence = true
+export const buildDate = '2022-10-13T12:14:22Z'
+export const useStoreLicence = false
