e39c2f3f23e6f995aba20bb643c9d942514ca92b
Darragh ORiordan <darragh.oriordan@gmail.com>
2023-10-01 18:44:33 +1100

    feat: revert auto change

--- src/electron/licencing/buildSettings.ts
+++ src/electron/licencing/buildSettings.ts
@@ -1,4 +1,4 @@
 // The contents of this file are overwritten by the release process.
 // *********************************************************************
 export const buildDate = '2023-10-01T05:41:21Z'
-export const useStoreLicence = false
+export const useStoreLicence = true
